d72704d68a98a2421c5f73acdc68200184be1577
Srivatsan <vatsan1993@gmail.com>
2023-10-03 21:05:39 +0530

    finished project

--- linked-list-timing.js
+++ linked-list-timing.js
@@ -7,3 +7,32 @@ Construct a timing test to verify the time complexities of `addToHead` and
 */
 
 // Your code here
+const timing = (cb, size) => {
+  const start = Date.now();
+  for (let i = 0; i < size; i++) {
+    cb(i);
+  }
+  return Date.now() - start;
+};
+
+let ll = new LinkedList();
+console.log('timings for single linked list addToHead');
+for (let size of [10, 100, 1000, 10000]) {
+  console.log(timing(ll.addToHead.bind(ll), size));
+}
+
+console.log('timings for single linked list addToTail');
+for (let size of [10, 100, 1000, 10000]) {
+  console.log(timing(ll.addToTail.bind(ll), size));
+}
+
+let dll = new DoublyLinkedList();
+console.log('timings for double linked list addToHead');
+for (let size of [10, 100, 1000, 10000]) {
+  console.log(timing(dll.addToHead.bind(dll), size));
+}
+
+console.log('timings for double linked list addToTail');
+for (let size of [10, 100, 1000, 10000]) {
+  console.log(timing(dll.addToTail.bind(dll), size));
+}
